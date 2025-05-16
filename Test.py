094eec97f8f7ffdac8d6f84ba0dea0b791c4cc80
Jamie Clynes <jamietclynes@hotmail.co.uk>
2025-05-16 19:49:55 +0100

    Random madlib for practice

---
+++ Test.py
@@ -0,0 +1,13 @@
+name = input("Enter a name: ")
+item = input("Enter an object: ")
+noise = input("Enter a noise: ")
+pt_verb = input("Enter a verb in the past tense: ")
+other_name = input("Enter another name: ")
+other_animal = input("Enter another animal: ")
+body_part = input("Enter a body part: ")
+
+
+print("There once was a dog named " + name + ".")
+print("He sat by himself on the " + item + ".")
+print("Suddenly he heard a loud " + noise + " and he " + pt_verb + " in panic.")
+print("Luckily " + other_name + " the " + other_animal + " came by and comforted him with a pat on the " + body_part + ".")
